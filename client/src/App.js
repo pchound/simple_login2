43a88ddbe87e26cd47b9235af121702771845157
pchound <82906754+pchound@users.noreply.github.com>
2024-02-13 11:43:20 -0700

    CRUD_tutorial_lesson1

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+
+function App() {
+ return (
+ <div className="App">
+    <h1>CRUD APPLICATION</h1>
+
+    <div className="form">
+      <label>Movie Name:</label>
+      <input type="text" name="movieName" />
+      <label>Review:</label>
+      <input type="text" name="review" />
+    </div>
+    <button>Submit</button>
+  </div>
+ );
+}
+
+export default App;

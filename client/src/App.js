6db994815e9cdf0eb1c97b1603c6759fdd463bac
pchound <82906754+pchound@users.noreply.github.com>
2024-02-19 16:16:27 -0700

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  const [movieName, setMovieName] = useState('')
+  const [review, setReview] = useState('')
+
  return (
  <div className="App">
     <h1>CRUD APPLICATION</h1>
 
     <div className="form">
       <label>Movie Name:</label>
-      <input type="text" name="movieName" />
+      <input type="text" name="movieName" onChange={(e)=>{
+        setMovieName(e.target.value)
+      }}/>
+
       <label>Review:</label>
-      <input type="text" name="review" />
+      <input type="text" name="review" onChange={(e)=>{
+        setReview(e.target.value)
+      }}/>
     </div>
     <button>Submit</button>
   </div>
--- server/index.js
+++ server/index.js
@@ -1,26 +1,87 @@
 const express = require("express");
 const app = express();
 const mysql = require('mysql')
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
 
 const db = mysql.createPool({
-    host: '127.0.0.1', // Use localhost or 127.0.0.1 as the hostname
-    port: '3307',      // Specify the port number
-    user: 'root',
-    password: 'password',
-    database: 'crud_database',
+  host: '127.0.0.1', // Use localhost or 127.0.0.1 as the hostname
+  port: '3307',      // Specify the port number
+  user: 'root',
+  password: 'password',
+  database: 'crud_database',
 });
 
+//Insert into movies
+app.post("/create", (req, res) => {
+  console.log("test", req.body)
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  db.query(
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)",
+    [movieName, movieReview],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+//Retrieve Movie results
 app.get("/movieReviews", (req, res) => {
-    db.query("SELECT * FROM movie_reviews", (err, result) => {
+  db.query("SELECT * FROM movie_reviews", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//Update movies
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+  console.log("test", id, movieName, movieReview);
+  db.query(
+    "UPDATE movie_reviews SET movieName = ?, movieReview = ? WHERE id = ?",
+    [movieName,movieReview, id],
+    (err, result) => {
       if (err) {
         console.log(err);
       } else {
         res.send(result);
       }
-    });
-  });
+    }
+  );
+});
+
+//Delete
+app.delete("/delete", (req, res) => {
+  const id = req.body.id;
+  db.query(
+    "DELETE FROM movie_reviews WHERE id = ?",
+    [id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
 
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });

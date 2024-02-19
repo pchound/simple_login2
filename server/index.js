41afaf4e6171f9cff4d08770efc73bce11644d15
pchound <82906754+pchound@users.noreply.github.com>
2024-02-19 13:06:46 -0700

    get_function

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql')
 
-app.get("/", (req, res) => {
-    res.send("hello");
+const db = mysql.createPool({
+    host: '127.0.0.1', // Use localhost or 127.0.0.1 as the hostname
+    port: '3307',      // Specify the port number
+    user: 'root',
+    password: 'password',
+    database: 'crud_database',
 });
 
-app.listen(3001, () => {
-    console.log("running on port 3001");
+app.get("/movieReviews", (req, res) => {
+    db.query("SELECT * FROM movie_reviews", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
